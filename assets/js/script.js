aeb87de8b00e2a72fe4a33b4cd9db447da661ff2
Kevin Grace <kgrace90@yahoo.com>
2021-11-15 20:54:40 -0500

    adjusted functionality to work properly

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,16 @@
 
 var userInitials = []; //user initial storage
 
-var begin = document.getElementById('start');
+var begin = document.getElementById('startBtn');
 
 var ul = document.getElementById('ul')
+
 var nextButton = document.getElementById('btnNext');
+
 var quizbox = document.getElementById('questionBox')
+
 var option1 = document.getElementById('option1')
+
 var option2 = document.getElementById('option2')
 
 var twoMinutes = 60 * 2,
@@ -16,7 +20,7 @@ var twoMinutes = 60 * 2,
 var quiz={
 
     questions:[
-
+        
         {
             qst:'How do you write a hidden comment in Javascript?',
             options: ['!-- --!', '//'],
@@ -26,7 +30,7 @@ var quiz={
             qst:'What does an Array do?',
             options: ['Holds multiple values into a single variable', 'Assigns values to variables'],
             answer:1
-        }
+        },
         {
             qst:'In HTML what is the proper tag name for adding images?',
             options: ['pic', 'img'],
@@ -36,94 +40,94 @@ var quiz={
             qst:'What does border-radius do?',
             options: ['Rounds the corners of the border', 'Increases the width of the border'],
             answer:1
-        },
+        }
     ],
-
-listQ:0,
-
-load:function(){
-    if(this.listQ<=this.questions.length-1){
-        quizbox.innerHTML=this.listQ+1 + ". " +this.questions[this.listQ].qst;
-        option1.innerHTML=this.questions[this.listQ].options[0];
-        option2.innerHTML=this.questions[this.listQ].options[1];
+    listQ:0,
+    
+    load:function(){
+        if(this.listQ<=this.questions.length-1){
+            quizbox.innerHTML=this.listQ+1 + ". " +this.questions[this.listQ].qst;
+            option1.innerHTML=this.questions[this.listQ].options[0];
+            option2.innerHTML=this.questions[this.listQ].options[1];
+        }
+        else {
+            
+            quizbox.innerHTML="You've reached the end!";
+            ul.style.display="none";
+            nextButton.style.display="none";
+        }
+    },
+    next: function(){
+        this.listQ++;
+        this.load();
+    },
+    
+    check: function(ele){
+        var id=ele.id.split('');
+        if(id[id.length-1]==this.questions[this.listQ].answer){
+            this.score++;
+            ele.className="correct";
+            this.scoreCard();
+        }
+        else{
+            ele.className="wrong";
+        }
+    },
+    
+    preventClick:function(){
+        for(let i=0; i<ul.children.length; i++){
+            ul.children[i].style.pointerEvents="none";
+        }
+    },
+    allowClick:function(){
+        for(let i=0; i<ul.children.length; i++){
+            ul.children[i].style.pointerEvents="auto";
+            ul.children[i].className=''
+        }
+    },
+    score:0,
+    scoreCard:function(){
+        scoreCard.innerHTML=this.questions.length + "|" + this.score;
     }
-    else {
-        
-        quizbox.innerHTML="You've reached the end!";
-        ul.style.display="none";
-        nextButton.style.display="none";
-    }
-},
-next: function(){
-    this.listQ++;
-    this.load();
-},
-
-check: function(ele){
-    var id=ele.id.split('');
-    if(id[id.length-1]==this.questions[this.listQ].answer){
-        this.score++;
-        ele.className="correct";
-        this.scoreCard();
-    }
-    else{
-        ele.className="wrong";
-    }
-},
-
-preventClick:function(){
-    for(let i=0; i<ul.children.length; i++){
-        ul.children[i].style.pointerEvents="none";
-    }
-},
-allowClick:function(){
-    for(let i=0; i<ul.children.length; i++){
-        ul.children[i].style.pointerEvents="auto";
-        ul.children[i].className=''
-    }
-},
-score:0,
-scoreCard:function(){
-    scoreCard.innerHTML=this.questions.length + "|" + this.score;
-}
 }
 
 function startQuiz(){
-timerCount(twoMinutes, display);
-quiz.load()
+    timerCount(twoMinutes, display);
+    quiz.load()
 }
 
 function button(ele){
-quiz.check(ele);
-quiz.preventClick();
+    quiz.check(ele);
+    quiz.preventClick();
 }
 
 function next(){
-quiz.next();
-quiz.allowClick();
+    quiz.next();
+    quiz.allowClick();
 }
+//Added countdown timer. Need to figure out how to subtract with wrong answers.
 
 function timerCount(duration, display) {
-var timer = duration, minutes, seconds;
-var counter = setInterval(function () {
-    minutes = parseInt(timer / 60, 10);
-    seconds = parseInt(timer % 60, 10);
+    var timer = duration, minutes, seconds;
+    var counter = setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
 
-    minutes = minutes < 10 ? "0" + minutes : minutes;
-    seconds = seconds < 10 ? "0" + seconds : seconds;
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
 
-    display.textContent = minutes + ":" + seconds;
+        display.textContent = minutes + ":" + seconds;
 
-    timer--
+        timer--
 
-    if (timer < 0) {
-        clearInterval(counter);
-    }
-}, 1000);
+        if (timer < 0) {
+            clearInterval(counter);
+        }
+    }, 1000);
 }
 
 var saveInitials = function() {
-localStorage.setItem("initials", JSON.stringify(userInitials));
+    localStorage.setItem("initials", JSON.stringify(userInitials));
 }
 
 begin.addEventListener("click", startQuiz);
